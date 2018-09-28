9e37bea18bfa25615bff6c1f93c28221da50c2a0
ldh756034624 <longdenghui@hotmail.com>
2018-09-28 10:32:57 +0800

    Update deliver.sh

--- deliver.sh
+++ deliver.sh
@@ -3,4 +3,4 @@
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-java -jar generate-id-0.0.1-SNAPSHOT.jar
+java -jar ./target/generate-id-0.0.1-SNAPSHOT.jar
